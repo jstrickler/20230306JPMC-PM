bd649fa7608dcc18737b93f7c8034d8160524796
jstrickler <jstrickler@gmail.com>
2023-03-09 20:03:05 +0000

    Thursday 3 PM

---
+++ alpha/mathlib/geometry.py
@@ -0,0 +1,14 @@
+from math import pi as PI
+
+def circle_area(diameter):
+    radius = diameter / 2
+    area = PI * radius ** 2
+    return area
+
+def rectangle_area(length, width):
+    return length * width
+
+def square_area(length):
+    return length ** 2
+
+
